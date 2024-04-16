7e4d36e2836af19d69ecf8e79c3cb6f8b221e6ae
Kybre <hermanchan10@gmail.com>
2024-04-16 03:19:13 -0700

    finalized home page, added akimbo corps page

--- scripts/content.js
+++ scripts/content.js
@@ -6,13 +6,13 @@ let otherData = [
 //		title:"Automagic",
 //		ctgr:"AI/Front-end Development Project",
 //		content:"[Page WIP]",
-//		tools:"Python/Dash/Cytoscape/SQLite",
+//		tools:"Python, Dash, Cytoscape, SQLite",
 //		link:"",
 //	},
 	{
 		img:"./img/Scrapbook/title.png",
 		title:"Scrapbook",
-		ctgr:"UI/UX Project",
+		ctgr:"UI / UX Project",
 		content:"A children's mental health journal created during Hack4Health 2021.",
 		tools:"Figma",
 		link:"scrapbook",
@@ -20,7 +20,7 @@ let otherData = [
 	{
 		img: "./img/KMSRT/kmsrt.png",
 		title: "Kalmont Mountain Safety and Rescue Team Website",
-		ctgr: "Web Design/Development Project",
+		ctgr: "Web Design / Development Project",
 		content: "A fictional website for the Kalmont Mountain Safety and Rescue Team Website containing local news and tips and tricks on winter outdoors safety.",
 		tools:"HTML/CSS",
 		link: "kmsrt"
@@ -31,23 +31,23 @@ let gameData = [
 	{
 		img:"./img/KartKlash/title.png",
 		title:"Kart Klash",
-		ctgr:"Game Design/Development Project",
-		content:"Kart Klash is a VR game that combines the thrilling experience of go-kart racing and the satisfaction of shooting targets and other racers.",
+		ctgr:"VR Design / Development Project",
+		content:"Kart Klash is a VR game that combines the thrilling experience of go-kart racing and the satisfaction of shooting targets and other racers, made for IAT 445 - Immersive Environments.",
 		tools:"Unity, C#",
 		link:"kart-klash.html",
 	},
-//	{
-//		img:"./img",
-//		title:"Akimbo Corps",
-//		ctgr:"Game Design/Development Project",
-//		content:"[Page WIP]",
-//		tools:"Unity, C#",
-//		link:"",
-//	},
+	{
+		img:"./img/Akimbo/title.png",
+		title:"Akimbo Corps",
+		ctgr:"Game Design / Development Project",
+		content:"Akimbo Corps is a top-down ",
+		tools:"Unity, C#",
+		link:"akimbo-corps.html",
+	},
 //	{
 //		img:"./img/GPPE/title.png",
 //		title:"The Great Peking Peak Escape",
-//		ctgr:"Game Design/Development Project",
+//		ctgr:"Game Design / Development Project",
 //		content:"A platformer centered around a grappling hook, where players must climb the walls of a kitchen to escape their cruel fate of becoming someone else's dinner. [Page WIP]",
 //		tools:"Unity, C#",
 //		link:"",
@@ -55,7 +55,7 @@ let gameData = [
 	{
 		img:"./img/ToA/cover.png", //image
 		title:"Tower of Ash",
-		ctgr:"Game Design/Development Project",
+		ctgr:"Game Design / Development Project",
 		content:"A roguelite metroidvania platformer developed in Unity for IAT 410 - Advanced Game Design.", //content
 		tools:"Unity, C#",
 		link:"tower-of-ash",
@@ -63,7 +63,7 @@ let gameData = [
 	{
 		img:"./img/Unfabled/cover.png",
 		title:"Unfabled",
-		ctgr:"Game Design/Development Project",
+		ctgr:"Game Design / Development Project",
 		content:"A fast-paced, spell-slinging action game developed in Unity for Ludum Dare 49. \n Rated Top 20% Overall in the Jam category.",
 		tools:"Unity, C#",
 		link:"unfabled",
@@ -71,7 +71,7 @@ let gameData = [
 	{
 		img:"./img/MonkeyMobius/title.png",
 		title:"Monkey Mobius",
-		ctgr:"Game Design/Development Project",
+		ctgr:"Game Design / Development Project",
 		content:"An infinite-themed puzzle game developed in Unity for the 2021 BC Game Jam. \n Winner of the Jam via the Participant's Choice award.",
 		tools:"Unity, C#",
 		link:"monkey-mobius",
@@ -79,7 +79,7 @@ let gameData = [
 	{
 		img:"./img/HellGene/title.png",
 		title:"Hell Gene",
-		ctgr:"Game Design/Development Project",
+		ctgr:"Game Design / Development Project",
 		content:"A chaos-themed, top-down hack and slash developed in Unity for Brackey's Game Jam 2021.2.",
 		tools:"Unity, C#",
 		link:"hell-gene",
@@ -87,7 +87,7 @@ let gameData = [
 	{
 		img: "./img/EiSC/title.png",
 		title: "Emergency in Section C",
-		ctgr: "Game Design/Development Project",
+		ctgr: "Game Design / Development Project",
 		content: "An emergency-themed platformer developed in Unity for the May 2021 Make-A-Level Game Jam.",
 		tools:"Unity, C#",
 		link: "emergency-in-section-c"
@@ -95,7 +95,7 @@ let gameData = [
 	{
 		img: "./img/Kingslayer/Kingslayer.png",
 		title: "Kingslayer",
-		ctgr: "Game Design/Development Project",
+		ctgr: "Game Design / Development Project",
 		content: "A high-action, platformer bullet hell game developed in Processing.",
 		tools:"Processing",
 		link: "kingslayer"
